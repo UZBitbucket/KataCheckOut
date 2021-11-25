d19a5a2ac7162ce134bbd666b1dbafe3e8200d63
umair zahid <sundarcampus@gmail.com>
2021-11-25 21:47:03 +0000

    Add project files.

---
+++ Kata.Domain/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Domain
+{
+    public class Product
+    {
+        public string SKU { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Kata.Domain/Models/SpecialOffer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kata.Domain.Models
+{
+    public class SpecialOffer
+    {
+        public string Item;
+        public int Quantity;
+        public decimal Reduction;
+    }
+}
